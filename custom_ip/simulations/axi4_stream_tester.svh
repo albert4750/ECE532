4f100b2ee3e2904118eaa4e22c61c19b482f6e9c
Yunhao Qian <yunhaoqian@hotmail.com>
2024-03-04 10:57:04 -0500

    Rename test -> tb and avoid using classes
    Vivado 2018.2 has bugs with virtual functions/tasks

--- custom_ip/simulations/axi4_stream_tester.svh
+++ custom_ip/simulations/axi4_stream_tester.svh
@@ -1,16 +1,15 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company:
 // Engineer: Yunhao Qian
 //
 // Create Date: 03/01/2024 12:42:54 PM
 // Design Name:
-// Module Name: axi4_stream_tester
+// Module Name:
 // Project Name: ECE532 Course Project - Real-Time Video Processing Pipeline
 // Target Devices: Nexys Video
 // Tool Versions: Vivado 2018.2
-// Description: This module is used to implement test benches for modules with
-//     AXI4-Stream inputs and outputs.
+// Description: This header file provides a template for modules that implement
+//     test benches for modules with AXI4-Stream inputs and outputs.
 //
 // Dependencies:
 //
@@ -21,35 +20,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-package axi4_stream_tester_pkg;
+`define AXI_STREAM_TESTER_RANDOM_PAUSE(clock, min_cycles, max_cycles) \
+    begin                                                             \
+        int pause_cycles = $urandom_range(min_cycles, max_cycles);    \
+        for (int i = 0; i < pause_cycles; ++i) begin                  \
+            @(negedge clock);                                         \
+        end                                                           \
+    end
 
-    virtual class test_data_base #(
-        int IN_DATA_WIDTH  = 8,
-        int OUT_DATA_WIDTH = 8
-    );
-
-        typedef logic [IN_DATA_WIDTH-1:0] in_data_t;
-        typedef logic [OUT_DATA_WIDTH-1:0] out_data_t;
-
-        pure virtual function void set_in_data(input int round, input int row, input int column,
-                                               output in_data_t tdata, output logic tlast);
-
-        pure virtual function void check_out_data(input int round, input int row, input int column,
-                                                  input out_data_t tdata, input logic tlast);
-
-    endclass : test_data_base
-
-    task automatic random_pause(ref logic clock, input int min_cycles, input int max_cycles);
-        int pause_cycles = $urandom_range(min_cycles, max_cycles);
-        for (int i = 0; i < pause_cycles; ++i) begin
-            @(negedge clock);
-        end
-    endtask : random_pause
-
-endpackage : axi4_stream_tester_pkg
-
-
-module axi4_stream_tester #(
+module `AXI_STREAM_TESTER_NAME #(
     int IN_DATA_WIDTH = 8,
     int OUT_DATA_WIDTH = 8,
     int IN_HEIGHT = 600,
@@ -69,13 +48,10 @@ module axi4_stream_tester #(
     input logic slave_tvalid_i,
     output logic slave_tready_o,
     input logic [OUT_DATA_WIDTH-1:0] slave_tdata_i,
-    input logic slave_tlast_i,
-
-    input axi4_stream_tester_pkg::test_data_base#(
-        .IN_DATA_WIDTH (IN_DATA_WIDTH),
-        .OUT_DATA_WIDTH(OUT_DATA_WIDTH)
-    ) test_data_i
+    input logic slave_tlast_i
 );
+
+    `AXI_STREAM_TESTER_IMPORT_TDATA_FUNCTIONS
 
     initial clock_o = 0;
     always #5 clock_o = ~clock_o;
@@ -89,11 +65,12 @@ module axi4_stream_tester #(
             $display("Started sending data for round %d", round);
             for (int row = 0; row < IN_HEIGHT; ++row) begin
                 for (int column = 0; column < IN_WIDTH; ++column) begin
-                    axi4_stream_tester_pkg::random_pause(clock_o, 0, 6);
+                    `AXI_STREAM_TESTER_RANDOM_PAUSE(clock_o, 0, 6);
 
                     // Send data to the DUT.
                     master_tvalid_o = 1;
-                    test_data_i.set_in_data(round, row, column, master_tdata_o, master_tlast_o);
+                    set_in_tdata(round, row, column, master_tdata_o);
+                    master_tlast_o = (row == IN_HEIGHT - 1) && (column == IN_WIDTH - 1);
                     do begin
                         @(posedge clock_o);
                     end while (!master_tready_i);
@@ -104,7 +81,7 @@ module axi4_stream_tester #(
             end
             $display("Finished sending data for round %d", round);
 
-            axi4_stream_tester_pkg::random_pause(clock_o, 0, 6);
+            `AXI_STREAM_TESTER_RANDOM_PAUSE(clock_o, 0, 6);
         end
 
         in_stream_finished = 1;
@@ -119,7 +96,7 @@ module axi4_stream_tester #(
             $display("Started receiving data for round %d", round);
             for (int row = 0; row < OUT_HEIGHT; ++row) begin
                 for (int column = 0; column < OUT_WIDTH; ++column) begin
-                    axi4_stream_tester_pkg::random_pause(clock_o, 0, 6);
+                    `AXI_STREAM_TESTER_RANDOM_PAUSE(clock_o, 0, 6);
 
                     // Receive data from the DUT.
                     slave_tready_o = 1;
@@ -128,7 +105,16 @@ module axi4_stream_tester #(
                     end while (!slave_tvalid_i);
 
                     // Check the data.
-                    test_data_i.check_out_data(round, row, column, slave_tdata_i, slave_tlast_i);
+                    check_out_tdata(round, row, column, slave_tdata_i);
+                    begin
+                        logic expected_tlast = (row == OUT_HEIGHT - 1) && (column == OUT_WIDTH - 1);
+                        assert (slave_tlast_i == expected_tlast)
+                        else begin
+                            $error(
+                                "Error: tlast, round=%d, row=%d, column=%d, expected=%d, actual=%d",
+                                round, row, column, expected_tlast, slave_tlast_i);
+                        end
+                    end
 
                     @(negedge clock_o);
                     slave_tready_o = 0;
@@ -136,7 +122,7 @@ module axi4_stream_tester #(
             end
             $display("Finished receiving data for round %d", round);
 
-            axi4_stream_tester_pkg::random_pause(clock_o, 0, 6);
+            `AXI_STREAM_TESTER_RANDOM_PAUSE(clock_o, 0, 6);
         end
 
         out_stream_finished = 1;
@@ -151,4 +137,6 @@ module axi4_stream_tester #(
         $finish;
     end
 
-endmodule
+endmodule : `AXI_STREAM_TESTER_NAME
+
+`undef AXI_STREAM_TESTER_RANDOM_PAUSE
