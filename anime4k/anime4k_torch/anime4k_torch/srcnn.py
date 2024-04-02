e8e1cc28af46febee47bc721495817118e5f0299
Guanghan Wang <xuanghdu@outlook.com>
2024-04-01 20:04:04 -0400

    Add more SRCNN config and update notebook

--- anime4k/anime4k_torch/anime4k_torch/srcnn.py
+++ anime4k/anime4k_torch/anime4k_torch/srcnn.py
@@ -26,15 +26,20 @@ class SRCNNModel(nn.Module):
         self.conv_layers = nn.ModuleList(
             [
                 # nn.Conv2d(input_depth, 64, kernel_size=9, padding=4),
-                # nn.Conv2d(64, 32, kernel_size=5, padding=2),
-                # # nn.Conv2d(64, 32, kernel_size=1, padding=0),
+                # # nn.Conv2d(64, 32, kernel_size=5, padding=2),
+                # nn.Conv2d(64, 32, kernel_size=1, padding=0),
                 # nn.Conv2d(32, input_depth, kernel_size=5, padding=2),
 
-                nn.Conv2d(input_depth, 8, kernel_size=3, padding=1),
-                nn.Conv2d(8, 8, kernel_size=3, padding=1),
-                nn.Conv2d(8, input_depth, kernel_size=3, padding=1),
+                nn.Conv2d(input_depth, 16, kernel_size=9, padding=4),
+                # nn.Conv2d(64, 32, kernel_size=5, padding=2),
+                nn.Conv2d(16, 8, kernel_size=1, padding=0),
+                nn.Conv2d(8, input_depth, kernel_size=5, padding=2),
 
-                # nn.Conv2d(input_depth, 9, kernel_size=8, padding='same'),
+                # nn.Conv2d(input_depth, 8, kernel_size=3, padding=1),
+                # nn.Conv2d(8, 8, kernel_size=3, padding=1),
+                # nn.Conv2d(8, input_depth, kernel_size=3, padding=1),
+
+                # nn.Conv2d(input_depth, 9, kernel_size=7, padding=3),
                 # nn.Conv2d(9, 12, kernel_size=1, padding=0),
                 # nn.Conv2d(12, input_depth, kernel_size=3, padding=1),
             ]
