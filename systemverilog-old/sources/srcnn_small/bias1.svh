ea772e5c691c91e8143f359a497a795b82289522
Yunhao Qian <yunhaoqian@hotmail.com>
2024-04-04 10:50:26 -0400

    Remove old SystemVerilog code

--- systemverilog-old/sources/srcnn_small/bias1.svh
+++
@@ -1,1 +0,0 @@
-localparam bit signed [0:7][33:0] Bias1 = '{34'd0, 34'd0, 34'd0, 34'd0, 34'd0, 34'd0, 34'd0, 34'd0};
--- systemverilog-old/sources/srcnn_small/bias2.svh
+++
@@ -1,1 +0,0 @@
-localparam bit signed [0:7][35:0] Bias2 = '{36'd0, 36'd0, 36'd0, 36'd0, 36'd0, 36'd0, 36'd0, 36'd0};
